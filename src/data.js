ab929d99f13a090d152094c0c9e2e08c63eeeb2a
alexandermaggs <75097869+alexandermaggs@users.noreply.github.com>
2021-07-07 09:42:13 +0100

    basic birthday reminder list, takes data and can clear all

---
+++ src/data.js
@@ -0,0 +1,40 @@
+import stock1 from "./assets/stock1.jpg";
+import stock2 from "./assets/stock2.jpg";
+import stock3 from "./assets/stock3.jpg";
+import stock4 from "./assets/stock4.jpg";
+import stock5 from "./assets/stock5.jpg";
+
+const DATA = [
+    {
+      id: 1,
+      name: 'Alexander Maggs',
+      age: 24,
+      image: stock1,
+    },
+    {
+      id: 2,
+      name: 'Eva Gravendale',
+      age: 32,
+      image: stock2,
+    },
+    {
+      id: 3,
+      name: 'Frank Hernandez',
+      age: 36,
+      image: stock3,
+    },
+    {
+      id: 4,
+      name: 'Tony Ford',
+      age: 34,
+      image: stock4,
+    },
+    {
+      id: 5,
+      name: 'Phillip Peters',
+      age: 29,
+      image: stock5,
+    },
+  ];
+
+  export default DATA;
